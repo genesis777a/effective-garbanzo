c7a203ea6f05609ce3e95781ae42733bde0484a6
genesis777a <genesis24ag@gmail.com>
2025-10-17 12:55:54 +0200

    Delete <🖤> directory

--- <🖤>/run.py
+++
@@ -1,8 +0,0 @@
-import pyximport
-pyximport.install(setup_args={"script_args" : ["--verbose"]})
-from bbox import compare_bboxes
-
-def main(args):
-   boxes1 = args.boxes1
-   boxes2 = args.boxes2
-   result = compare_bboxes(boxes1, boxes2)
